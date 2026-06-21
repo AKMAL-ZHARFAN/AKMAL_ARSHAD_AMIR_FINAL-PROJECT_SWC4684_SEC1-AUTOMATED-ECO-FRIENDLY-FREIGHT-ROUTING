f93e1416f1da7e374177ad55515c8b270701f23e
AKMAL-ZHARFAN <kmlzharfan@gmail.com>
2026-06-21 15:50:42 +0800

    Update Main_Method.java

--- Main_Method.java
+++ Main_Method.java
@@ -1,8 +1,10 @@
 public class Main_Method{
     public static void main(String[]args){
 
+        
         Delivery_Main_Engine Engine = new Delivery_Main_Engine();
 
+        //Run the entire system
         Engine.System_ON();
 
     }
