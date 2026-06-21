4f9ad7b56e638dbb00f684c68282c2f06d66614c
AKMAL-ZHARFAN <kmlzharfan@gmail.com>
2026-06-21 15:41:28 +0800

    Update Courier_Info.java

--- Courier_Info.java
+++ Courier_Info.java
@@ -1,16 +1,19 @@
 import java.util.LinkedList;
 public class Courier_Info{
+    //class Instance
     String carrierId;
     String carrierName;
     String fleetType;
-
+   //To store shipment info inside each courier
     LinkedList<ShipmentInfo> items = new LinkedList<>();
 
+    //Constructor
     Courier_Info(String ID, String Name, String Type, String Item_ID, String Item_Type, String Score, String Date, int Time, int Tax){
         carrierId = ID;
         carrierName =  Name;
         fleetType = Type;
-
+        
+       //each time Courier Info object is created, it will automatically add shipment info inside LinkedList in Courier_Info class
         items.add(new ShipmentInfo( Item_ID, Item_Type,  Score, Date,  Time,  Tax));
 
     }
